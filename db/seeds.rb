dffbf09e3dd65333f2f65fb4d9805a66405369e2
Jimmy Falco <juanfalco@hotmail.com.ar>
2022-05-06 01:11:20 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
     Company.create([{name: "Jimmy SA", token: "58a0e38d-2c74-4fe5-b013-abb4b606747d"},{name: "Burgers SA", token: "510631c8-99d6-44c7-a912-e0363085cdd5"}])
     Candidate.create([{name: "Jimmy"},{name: "Teo"}])
-    WorkNotification.create([{name: "Programmer", company_id: "1"},{name: "RRHH" company_id: "1"},{name: "Management", company_id: "2"}])
+    WorkNotification.create([{name: "Programmer", company_id: "1"},{name: "RRHH", company_id: "1"},{name: "Management", company_id: "2"}])
+    StatusWorkNotification.create([{candidate_id: "1",work_notification_id:"1"},{candidate_id: "2",work_notification_id:"3"}])
 
 
